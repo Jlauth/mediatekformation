415c2456dd14363ad97bf86d8f36764fc8b91e95
Jlauth <lauth_jean@live.fr>
2022-12-03 19:09:10 +0100

    Merge pull request #25 from Jlauth/master
    M3 - T1 : Test fonctionnel ok, tests d'intégration revus et à revoir.

--- tests/Controller/AccueilControllerTest.php
+++ tests/Controller/AccueilControllerTest.php
@@ -10,15 +10,69 @@ use Symfony\Component\HttpFoundation\Response;
  *
  * @author Jean
  */
-class AccueilControllerTest extends WebTestCase{
-    
+class AccueilControllerTest extends WebTestCase {
+
     /**
      * Initialisation du client de test d'accès à la page d'accueil
      */
-    public function testAccessPage(){
+    public function testAccessPageAccueil() {
         $client = static::createClient();
         $client->request('GET', '/');
-        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        /** @var type $response */
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    /**
+     * Test link formations depuis Accueil
+     */
+    public function testLinkFormationsAccueil() {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $client->clickLink('Formations');
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $uri = $client->getRequest()->server->get("REQUEST_URI");
+        $this->assertEquals('/formations', $uri);
+    }
+
+    /**
+     * Test link playlists depuis Accueil
+     */
+    public function testLinkPlaylistsAccueil() {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $client->clickLink('Playlists');
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $uri = $client->getRequest()->server->get("REQUEST_URI");
+        $this->assertEquals('/playlists', $uri);
+    }
+
+    /**
+     * Test link CGU depuis Accueil
+     */
+    public function testLinkCguAccueil() {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $client->clickLink('Conditions Générales d\'Utilisation');
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $uri = $client->getRequest()->server->get("REQUEST_URI");
+        $this->assertEquals('/cgu', $uri);
     }
     
+    /**
+     * Test link accueil depuis Accueil
+     */
+    public function testLinkAccueil(){
+         $client = static::createClient();
+        $client->request('GET', '/');
+        $client->clickLink('Accueil');
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $uri = $client->getRequest()->server->get("REQUEST_URI");
+        $this->assertEquals('/', $uri);
+    }
+
 }
--- tests/Controller/PlaylistsControllerTest.php
+++ tests/Controller/PlaylistsControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Description of PlaylistsControllerTest
@@ -10,6 +11,64 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  * @author Jean
  */
 class PlaylistsControllerTest extends WebTestCase {
+
+    /**
+     * Initialisation du client de test d'accès à la page playlists
+     */
+    public function testAccessPage() {
+        $client = static::createClient();
+        $client->request('GET', '/playlists');
+        /** @var type $response */
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    /**
+     * Test filtre playlist name dans Playlists
+     */
+    public function testFiltrePlaylistName() {
+        $client = static::createClient();
+        $client->request('GET', '/playlists/recherche/{champ}/{table}');
+        $crawler = $client->submitForm('filtrer', [
+            'recherche' => 'Cours'
+        ]);
+        $this->assertCount(33, $crawler->filter('h5'));
+        $this->assertSelectorTextContains('h5', 'Cours');
+    }
     
+    /**
+     * Test tri categorie dans Playlists
+     */
+    public function testFiltrePlaylistCategorie(){
+        $client = static::createClient();
+        $client->request('GET', '/playlists/recherche/{champ}/{table}');
+        $crawler = $client->submitForm('filtrer', [
+            'recherche' => 'Android'
+        ]);
+        $this->assertCount(6, $crawler->filter('h5'));
+        $this->assertSelectorTextContains('h5', 'Android');
+    }
+
+    /**
+     * Test tri formation dans Playlists
+     */
+    public function testTriPlaylistNbFormations(){
+        $client = static::createClient();
+        $client->request('GET', '/playlists/recherche/{champ}/{table}');
+        $crawler = $client->submitForm('filtrer');
+        $this->assertCount(81, $crawler->filter('h5'));
+    }
     
+    /**
+     * Test sur la réponse du clic bouton
+     */
+    public function testLinkPlaylist() {
+        $client = static::createClient();
+        $client->request('GET', '/playlists');
+        $client->clickLink('Voir détail');
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $uri = $client->getRequest()->server->get("REQUEST_URI");
+        $this->assertEquals('/playlists/playlist/13', $uri);
+    }
 }
